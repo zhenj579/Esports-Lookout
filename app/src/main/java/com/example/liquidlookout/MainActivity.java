0e588956ff994bff419417e79f8a19388bb7b9e7
RN1372 <richardngowork@gmail.com>
2020-11-06 21:52:19 -0500

    added a onclick function in the main activity java

--- app/src/main/java/com/example/liquidlookout/MainActivity.java
+++ app/src/main/java/com/example/liquidlookout/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.liquidlookout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button DiffGamesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DiffGamesButton = findViewById(R.id.gamesButton);
+    }
+    public void ChangeScreen(View v){
+        if(v == DiffGamesButton){
+        }
     }
     //test commit Richard
 }
