5bc1734b8b4fb0ab5a73bee7fa0b03f4dd804fda
John Zhen <john.zhen24@myhunter.cuny.edu>
2020-11-07 01:08:21 -0500

    swapped volley out and used a traditional httprequest instead

--- app/src/main/java/com/example/liquidlookout/MainActivity.java
+++ app/src/main/java/com/example/liquidlookout/MainActivity.java
@@ -4,62 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button DiffGamesButton;
-    Button subMatchesButton;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DiffGamesButton = findViewById(R.id.gamesButton);
-        subMatchesButton = findViewById(R.id.subscribedButton);
+        JSONParser parser = new JSONParser("https://api.pandascore.co/teams/liquid-cs-go/tournaments?token=l5U9gyKracl0VKg_p-73677Gd9aOsNdduej6R0lEVPXhQu-5rbQ");
+        Thread t = new Thread(parser);
+        t.start();
     }
-    public void ChangeScreen(View v){
-        if(v == DiffGamesButton){
+
+    public void ChangeScreen(View v) {
+        if (v == DiffGamesButton) {
             Intent DiffGamesIntent = new Intent(this, DifferentGamesActivity.class);
             startActivity(DiffGamesIntent);
         }
-        else if(v == subMatchesButton)
-        {
-            Intent subMatchesIntent = new Intent(this, SubscribedMatchesActivity.class);
-            startActivity(subMatchesIntent);
-        }
     }
-    private List<String> getJSONStrings(final List<String> urls)
-    {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        final List<String> responses = new ArrayList<>();
-        for(final String url : urls)
-        {
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    responses.add(response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.e("error", error.toString());
-                }
-            });
-            queue.add(stringRequest);
-        }
-        return responses;
-    }
+
     //test commit Richard
 }
