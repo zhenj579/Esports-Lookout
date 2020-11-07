d06ebc1e8b94844a2971373d7e194a6dbaad0dc1
RN1372 <richardngowork@gmail.com>
2020-11-06 22:33:25 -0500

    added DifferentGames activity

---
+++ app/src/main/java/com/example/liquidlookout/DifferentGamesActivity.java
@@ -0,0 +1,16 @@
+package com.example.liquidlookout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class DifferentGamesActivity extends AppCompatActivity {
+    Button DiffGamesHomeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.different_games_activity);
+    }
+}
--- app/src/main/java/com/example/liquidlookout/MainActivity.java
+++ app/src/main/java/com/example/liquidlookout/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.liquidlookout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button DiffGamesButton;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void ChangeScreen(View v){
         if(v == DiffGamesButton){
+            Intent DiffGamesIntent = new Intent(this, DifferentGamesActivity.class);
+            startActivity(DiffGamesIntent);
         }
     }
     //test commit Richard
