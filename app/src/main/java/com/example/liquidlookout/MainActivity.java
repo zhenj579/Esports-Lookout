8fa9119f02bab2e4a5df555b0769b8b3886c2514
John Zhen <john.zhen24@myhunter.cuny.edu>
2020-11-07 00:02:03 -0500

    added function to convert json from url to string

--- app/src/main/java/com/example/liquidlookout/MainActivity.java
+++ app/src/main/java/com/example/liquidlookout/MainActivity.java
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button DiffGamesButton;
@@ -21,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
             Intent DiffGamesIntent = new Intent(this, DifferentGamesActivity.class);
             startActivity(DiffGamesIntent);
         }
+    }
+    private List<String> getJSONStrings(final List<String> urls)
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        final List<String> responses = new ArrayList<>();
+        for(final String url : urls)
+        {
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    responses.add(response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("error", error.toString());
+                }
+            });
+            queue.add(stringRequest);
+        }
+        return responses;
     }
     //test commit Richard
 }
