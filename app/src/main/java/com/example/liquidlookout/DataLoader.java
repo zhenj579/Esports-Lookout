ee771bebec67371fa7adf00ff40bf06f4702a1fc
John Zhen <john.zhen24@myhunter.cuny.edu>
2020-11-07 23:05:19 -0500

    removed unused lines of code and utilized game enum

--- app/src/main/java/com/example/liquidlookout/DataLoader.java
+++ app/src/main/java/com/example/liquidlookout/DataLoader.java
@@ -32,17 +32,14 @@ public class DataLoader{
     public static final String lolUrl = api + "lol/matches/upcoming?";
     public static final String csUrl = api + "csgo/matches/upcoming?";
     public static final String team = api + "teams/";
-    public static final String searchRange = "range[begin_at]="+today.getYear() + "-" + today.getMonth().getValue() + "-" + (today.getDayOfMonth() < 10 ? "0"+ today.getDayOfMonth() : today.getDayOfMonth()) +",3000-12-26&";
     public static final String token = "token=l5U9gyKracl0VKg_p-73677Gd9aOsNdduej6R0lEVPXhQu-5rbQ";
 
     private static DataLoader dt = null;
-    private static Thread loader = null;
 
     private static boolean loaded = false;
 
     private LOL lol;
     private CSGO csgo;
-    private JSONParser parser;
 
     private static ExecutorService es = Executors.newFixedThreadPool(2);
 
@@ -186,11 +183,11 @@ public class DataLoader{
 
         if(g == Game.LOL)
         {
-            url = getWebPageAsString(lolUrl + searchRange + token);
+            url = getWebPageAsString(lolUrl + token);
         }
         else if(g == Game.CSGO)
         {
-            url = getWebPageAsString(csUrl + searchRange + token);
+            url = getWebPageAsString(csUrl + token);
         }
         try {
             JSONArray jsonArr = new JSONArray(url);
