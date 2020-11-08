ce7ab5cceea6f3495818961498c6909131d43092
John Zhen <john.zhen24@myhunter.cuny.edu>
2020-11-08 01:28:29 -0500

    renamed and refactored classes

--- app/src/main/java/com/example/liquidlookout/Game.java
+++ app/src/main/java/com/example/liquidlookout/Game.java
@@ -1,5 +1,26 @@
 package com.example.liquidlookout;
 
-public enum Game {
-    CSGO,LOL
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+//games object to hold a string name and an int that holds the location of the picture in the drawable folder
+public class Game {
+    private String name;
+    private int img;
+
+    public Game(String name, int url, Games game) {
+        this.name = name;
+        this.img = url;
+    }
+
+    public int getImgUrl() {
+        return img;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/example/liquidlookout/Games.java
+++ app/src/main/java/com/example/liquidlookout/Games.java
@@ -1,29 +1,5 @@
 package com.example.liquidlookout;
 
-//games object to hold a string name and an int that holds the location of the picture in the drawable folder
-public class Games {
-    private String name;
-    private int img;
-
-    public Games(String name,int url) {
-
-        this.name = name;
-        this.img = url;
-    }
-
-    public int getImgUrl() {
-        return img;
-    }
-
-    public void setImgUrl(int imgUrl) {
-        this.img = imgUrl;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+public enum Games {
+    CSGO,LOL,DOTA2,OVERWATCH
 }
